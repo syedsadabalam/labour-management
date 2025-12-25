84bccc0740093d2e5d9895f4d54e03882ef48ca1
Syed Sadab Alam <syedsadab29@gmail.com>
2025-12-26 01:26:45 +0530

    Fix gunicorn entrypoint

--- app.py
+++ app.py
@@ -35,8 +35,10 @@ def create_app():
 
     return app
 
+app = create_app()
 
 if __name__ == "__main__":
     app = create_app()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
